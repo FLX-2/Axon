4c997605071494e478931c189aa2332a5d9d55e8
Eli <emora4284@gmail.com>
2025-07-09 17:03:14 -0700

    style: update title bar buttons to match standard look

--- src/components/Titlebar.tsx
+++ src/components/Titlebar.tsx
@@ -17,19 +17,19 @@ export const Titlebar: React.FC = () => {
           onClick={() => appWindow.minimize()}
           className="h-8 w-12 flex items-center justify-center hover:bg-buttonHover"
         >
-          <Minus className="w-4 h-4 text-iconSecondary" />
+          <Minus className="w-3.5 h-3.5 text-iconSecondary" />
         </button>
         <button
           onClick={() => appWindow.toggleMaximize()}
           className="h-8 w-12 flex items-center justify-center hover:bg-buttonHover"
         >
-          <Square className="w-4 h-4 text-iconSecondary" />
+          <Square className="w-3.5 h-3.5 text-iconSecondary" />
         </button>
         <button
           onClick={() => appWindow.hide()}
-          className="h-8 w-12 flex items-center justify-center hover:bg-buttonHover text-red-500"
+          className="h-8 w-12 flex items-center justify-center hover:bg-buttonHover"
         >
-          <X className="w-4 h-4" />
+          <X className="w-3.5 h-3.5 text-iconSecondary" />
         </button>
       </div>
     </div>
